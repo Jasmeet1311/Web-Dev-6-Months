9263e330a14d0f361f2f250bdc44c978d865b97f
Jasmeet Kaur <jasmeet13112001@gmail.com>
2024-03-01 12:39:07 +0530

    JS loops

---
+++ Js_loops.js
@@ -0,0 +1,24 @@
+// Loops in Javascript
+// for loop
+// for (let i = 0; i < 10; i++) {
+//     console.log(i); 
+// }
+
+let obj ={
+    name:"Jasmeet",
+    age:22,
+    "Work Id": 67890
+}
+// for in loop  --> to print obj
+// for (const key in obj) {
+//         const element = obj[key];
+//         console.log(key,element);
+        
+// }
+// for of loop --> to print iterables
+for (const c of "Jasmeet") {
+    console.log(c);
+}
+
+// while
+// do while 
